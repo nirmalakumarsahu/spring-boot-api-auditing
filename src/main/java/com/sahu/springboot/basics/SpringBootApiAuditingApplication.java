6264ae0caa2c5d63dca4773a30d82bf7a4a52f91
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2026-01-19 23:54:57 +0530

    Project Structure Added

---
+++ src/main/java/com/sahu/springboot/basics/SpringBootApiAuditingApplication.java
@@ -0,0 +1,13 @@
+package com.sahu.springboot.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiAuditingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiAuditingApplication.class, args);
+	}
+
+}
